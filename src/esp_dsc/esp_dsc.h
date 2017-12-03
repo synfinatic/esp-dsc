8354e35742b9af8b9017a04e4fa8faed580e6048
Aaron Turner <synfinatic@gmail.com>
2017-12-03 14:26:26 -0800

    v1.1 board & code
    - Move GPIO2/15 to GPIO14/12 to avoid boot conflict
    - Remove level shifter and replace with 4x 150Ohm resistors
    - Tweak RJ45 footprint for better fit
    - Add pullup for D+ on USB
    - Remove 3.3V FTDI jumper
    - Move Reset button label
    - FTDI no longer can provide power

--- src/esp_dsc/esp_dsc.h
+++ src/esp_dsc/esp_dsc.h
@@ -16,10 +16,10 @@
 /* Encoder pins on RJ45 */
 #define CHAN_DEC_A   5     // RJ45 pin 7
 #define CHAN_DEC_B   4     // RJ45 pin 5
-#define CHAN_RA_A    2     // RJ45 pin 3
-#define CHAN_RA_B   15     // RJ45 pin 1
+#define CHAN_RA_A    14    // RJ45 pin 3
+#define CHAN_RA_B    12    // RJ45 pin 1
 
-#define ESP_DSC_VERSION "0.0.2"
+#define ESP_DSC_VERSION "1.0.0"
 #define MAX_SRV_CLIENTS 4
 #define MAX_AP_RETRIES 60
 #define SERIAL_SPEED 115200
